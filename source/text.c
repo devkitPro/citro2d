76658bd93c3c793aa774cf8242b3b7b273ac1e39
piepie62 <christopherfeger@yahoo.com>
2020-05-05 02:26:49 -0400

    Fix small inconsistency in alignment

--- source/text.c
+++ source/text.c
@@ -422,7 +422,8 @@ void C2D_DrawText(const C2D_Text* text, u32 flags, float x, float y, float z, fl
 				}
 
 				// Set up final word beginnings and ends
-					words[0].xEnd = words[0].end->xPos + words[0].end->width - words[0].start->xPos;
+				words[0].xBegin = x;
+				words[0].xEnd = words[0].end->xPos + words[0].end->width - words[0].start->xPos;
 				for (u32 i = 1; i < text->words; i++)
 				{
 					if (words[i-1].start->lineNo != words[i].start->lineNo)
@@ -441,7 +442,7 @@ void C2D_DrawText(const C2D_Text* text, u32 flags, float x, float y, float z, fl
 					u32 consecutiveWordNum = cur->wordNo + lines[cur->lineNo].wordStart;
 					float glyphW = scaleX*cur->width;
 					// Specified X value plus the scaled beginning offset plus the scaled offset from the beginning plus the calculated whitespace width
-					float glyphX = x + scaleX*words[consecutiveWordNum].xBegin + scaleX*(cur->xPos - words[consecutiveWordNum].start->xPos); // scaleX*cur->xPos + scaleX*whitespaceWidth[cur->lineNo]*cur->wordNo
+					float glyphX = scaleX*words[consecutiveWordNum].xBegin + scaleX*(cur->xPos - words[consecutiveWordNum].start->xPos); // scaleX*cur->xPos + scaleX*whitespaceWidth[cur->lineNo]*cur->wordNo
 					float glyphY = y + dispY*cur->lineNo;
 
 					C2Di_SetTex(cur->sheet);
